6ee4febe92f912e05a05122f850c5bda95c0f047
Mukund Gupta <mukundofficial07@gmail.com>
2021-07-12 13:53:44 +0530

    Updates

---
+++ precache-manifest.e4c4dbea8e580bd9e2ea8d2acc6fe6f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f12e754eabfd355a88b5",
+    "url": "/build/static/css/main.1f936b11.chunk.css"
+  },
+  {
+    "revision": "f12e754eabfd355a88b5",
+    "url": "/build/static/js/main.f12e754e.chunk.js"
+  },
+  {
+    "revision": "24a24dbc93ab41b562f3",
+    "url": "/build/static/js/1.24a24dbc.chunk.js"
+  },
+  {
+    "revision": "3923d73d01a4271659cc",
+    "url": "/build/static/js/runtime~main.3923d73d.js"
+  },
+  {
+    "revision": "bed3b71338083f5c05a0010cf702ccc2",
+    "url": "/build/index.html"
+  }
+];
